b907b8fef66b13dd7baad0f397e30915d4245e0e
Ilia Korvigo <ilia.korvigo@gmail.com>
2017-08-14 12:06:51 +0300

    Removing cython compilation from setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from setuptools import find_packages
 from distutils.core import setup
 import os
 
-from Cython.Build import cythonize
-
-os.environ['CFLAGS'] = '-O3 -Wall'
+# from Cython.Build import cythonize
+#
+# os.environ['CFLAGS'] = '-O3 -Wall'
 
 setup(
     name="chempred",
-    ext_modules=cythonize(["chempred/chemdner.pyx"]),
+    # ext_modules=cythonize(["chempred/chemdner.py"]),
     packages=find_packages("./"),
     requires=["numpy",
-              "cython",
-              "fn"]
+              # "cython",
+              "fn",
+              "pyrsistent"]
 )

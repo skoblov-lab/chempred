6afd48c1f8e3a4314450e834c3fa60fb1a7ac972
Ilia Korvigo <ilia.korvigo@gmail.com>
2017-08-16 21:52:15 +0300

    bugfix

--- chempred/model.py
+++ chempred/model.py
@@ -9,60 +9,60 @@ from functools import reduce
 from itertools import chain
 from fn import F
 
-# from keras import layers
+from keras import layers
 import numpy as np
 
 from chempred.chemdner import Interval
 
 
-# def stack_conv(prev: layers.Layer, param: Tuple[str, int, int]):
-#     name, nfilt, kern_size = param
-#     return layers.Convolution1D(
-#         nfilt, kern_size, activation="relu", name=name,
-#     )(prev)
-#
-#
-# def build_conv(incomming,
-#                filters: Optional[Sequence[int]],
-#                kernels: Optional[Sequence[int]]):
-#     filters = filters or []
-#     kernels = kernels or []
-#     assert len(filters) == len(kernels)
-#
-#     conv_names = ("conv_{}".format(i) for i in range(1, len(kernels)+1))
-#     conv = reduce(stack_conv, zip(conv_names, filters, kernels), incomming)
-#     return conv
-#
-#
-# def stack_lstm(prev: layers.Layer, param: Tuple[str, int, float, float],
-#                bidirectional: bool, stateful: bool):
-#     name, units, indrop, recdrop = param
-#     layer = layers.LSTM(units, dropout=indrop, recurrent_dropout=recdrop,
-#                         return_sequences=True, stateful=stateful)
-#     return (layers.Bidirectional(layer) if bidirectional else layer)(prev)
-#
-#
-# def build_rec(nsteps: Optional[Sequence[int]],
-#               lstm_inp_drop: Optional[Sequence[float]],
-#               lstm_rec_drop: Optional[Sequence[float]],
-#               bidirectional=True,
-#               stateful=False):
-#
-#     nsteps = nsteps or []
-#     lstm_inp_drop = lstm_inp_drop or []
-#     lstm_rec_drop = lstm_rec_drop or []
-#     assert len(nsteps) == len(lstm_rec_drop) == len(lstm_inp_drop)
-#
-#     def rec(incomming):
-#         rec_names = ("rec_{}".format(i) for i in range(1, len(nsteps)+1))
-#         recur = reduce(
-#             F(stack_lstm, bidirectional=bidirectional, stateful=stateful),
-#             zip(rec_names, nsteps, lstm_inp_drop, lstm_rec_drop),
-#             incomming
-#         )
-#         return recur
-#
-#     return rec
+def stack_conv(prev: layers.Layer, param: Tuple[str, int, int]):
+    name, nfilt, kern_size = param
+    return layers.Convolution1D(
+        nfilt, kern_size, activation="relu", name=name,
+    )(prev)
+
+
+def build_conv(incomming,
+               filters: Optional[Sequence[int]],
+               kernels: Optional[Sequence[int]]):
+    filters = filters or []
+    kernels = kernels or []
+    assert len(filters) == len(kernels)
+
+    conv_names = ("conv_{}".format(i) for i in range(1, len(kernels)+1))
+    conv = reduce(stack_conv, zip(conv_names, filters, kernels), incomming)
+    return conv
+
+
+def stack_lstm(prev: layers.Layer, param: Tuple[str, int, float, float],
+               bidirectional: bool, stateful: bool):
+    name, units, indrop, recdrop = param
+    layer = layers.LSTM(units, dropout=indrop, recurrent_dropout=recdrop,
+                        return_sequences=True, stateful=stateful)
+    return (layers.Bidirectional(layer) if bidirectional else layer)(prev)
+
+
+def build_rec(nsteps: Optional[Sequence[int]],
+              lstm_inp_drop: Optional[Sequence[float]],
+              lstm_rec_drop: Optional[Sequence[float]],
+              bidirectional=True,
+              stateful=False):
+
+    nsteps = nsteps or []
+    lstm_inp_drop = lstm_inp_drop or []
+    lstm_rec_drop = lstm_rec_drop or []
+    assert len(nsteps) == len(lstm_rec_drop) == len(lstm_inp_drop)
+
+    def rec(incomming):
+        rec_names = ("rec_{}".format(i) for i in range(1, len(nsteps)+1))
+        recur = reduce(
+            F(stack_lstm, bidirectional=bidirectional, stateful=stateful),
+            zip(rec_names, nsteps, lstm_inp_drop, lstm_rec_drop),
+            incomming
+        )
+        return recur
+
+    return rec
 
 
 def merge_predictions(intervals: List[Interval], predictions: np.ndarray) \

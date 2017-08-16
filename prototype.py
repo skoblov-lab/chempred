75572929ae07b1d272a9cb11daa94af2872c5f0d
Ilia Korvigo <ilia.korvigo@gmail.com>
2017-08-16 21:47:29 +0300

    CLI prototype

--- prototype.py
+++ prototype.py
@@ -104,9 +104,31 @@ def prepare_training_data(abstracts: str, annotations: str, window: int,
             joined_classes, masks_text)
 
 
-def main():
+@click.group("chemdpred", help=__doc__)
+@click.option("-m", "--models",
+              help="Path to a directory with ChemPred models.")
+# @click.option("-a", "--abstracts", default=None)
+# @click.option("-t", "--annotations", default=None)
+@click.pass_context
+def chempred(ctx, models):
+    pass
+
+
+@chempred.group("train")
+@click.option("-t", "--tagger",
+              help="Configurations for the chemical entity tagging model; "
+                   "the model will not be trained without them.")
+@click.option("-d", "--detector",
+              help="Configurations for the chemical entity detection model; "
+                   "the model will not be trained without them.")
+def train(ctx, segmentation, classifier):
+    pass
+
+
+@chempred.group("annotate")
+def annotate(ctx):
     pass
 
 
 if __name__ == "__main__":
-    main()
+    chempred()

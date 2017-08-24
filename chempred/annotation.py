6d4beae5e5aca55d672b2b5a926bdaa230c464e7
Ilia Korvigo <ilia.korvigo@gmail.com>
2017-08-24 12:49:56 +0300

    Minor annotation API change

--- chempred/annotation.py
+++ chempred/annotation.py
@@ -92,33 +92,18 @@ class Annotation:
         Interval(start=2, end=3)
         >>> annotation[-1]
         Interval(start=19, end=30)
-        >>> [(i.start, i.end) for i in annotation[4:17]] == ranges[1:4]
-        True
-        >>> annotation[0:2]
-        []
-        >>> annotation[30:]
-        Traceback (most recent call last):
-        ...
-        ValueError: Implicit slicing is not supported - specify both borders
         >>> annotation["a"]
         Traceback (most recent call last):
         ...
-        ValueError: Can't use objects of type str for indexing/slicing
+        TypeError: Can't use objects of type str for indexing/slicing
         """
-        if isinstance(item, slice):
-            start, stop = item.start, item.stop
-            if start is None or stop is None:
-                raise ValueError("Implicit slicing is not supported - specify "
-                                 "both borders")
-            first, last = self.borders(start, stop)
-            if first is None or last is None or not last - first:
-                return []
-            return self._regions[first:last+1]
-        elif isinstance(item, int):
-            return self._regions[item] if item < self.size else None
-
-        raise ValueError("Can't use objects of type {} for "
-                         "indexing/slicing".format(type(item).__name__))
+        try:
+            return self._regions[item]
+        except IndexError:
+            raise IndexError("{} is out of bounds".format(item))
+        except TypeError:
+            raise TypeError("Can't use objects of type {} for "
+                            "indexing/slicing".format(type(item).__name__))
 
     def __bool__(self) -> bool:
         return bool(self._regions)
@@ -131,61 +116,81 @@ class Annotation:
     def size(self) -> int:
         return len(self._regions)
 
-    def borders(self, start: Integral, stop: Integral) \
-            -> Tuple[Optional[int], Optional[int]]:
+    def lookup(self, start: Integral, stop: Integral) -> List[Interval]:
         # TODO docs
         """
-        :param start: inclusive left-border
-        :param stop: non-inclusive right-border
-        :return: !!!Both borders are inclusive!!!
+        :param start:
+        :param stop:
+        :return:
         >>> from itertools import starmap
         >>> ranges = [(2, 3), (5, 6), (7, 9), (11, 15), (19, 30)]
         >>> regions = list(starmap(Interval, ranges))
         >>> annotation = Annotation(regions)
-        >>> annotation.borders(4, 17)
-        (1, 3)
-        >>> annotation.borders(2, 12)
-        (0, 3)
-        >>> annotation.borders(0, 17)
-        (0, 3)
-        >>> annotation.borders(0, 25)
-        (0, 4)
-        >>> annotation.borders(0, 40)
-        (0, 4)
-        >>> annotation.borders(0, 2)
-        (0, 0)
-        >>> annotation.borders(3, 4)
-        (1, 1)
-        >>> annotation.borders(40, 50)
-        (None, None)
+        >>> [(i.start, i.end) for i in annotation.lookup(4, 17)] == ranges[1:4]
+        True
+        >>> annotation.lookup(0, 2)
+        []
         """
-        final = self.size - 1
+        first, last = self.borders(start, stop)
+        if first is None or last is None or not last - first:
+            return []
+        return self._regions[first:last + 1]
 
-        @tco
-        def left_border(l, idx):
-            if self[idx].contains(l) or (not idx and self[idx].after(l)):
-                return False, idx
-            if idx >= final and self[idx].before(l):
-                return False, None
-            if self[idx].before(l) and self[idx+1].after(l):
-                return False, idx + 1
-            return True, (l, (idx // 2 if self[idx].after(l) else
-                              ceil((idx + final) / 2)))
+    def borders(self, start: Integral, stop: Integral) \
+                -> Tuple[Optional[int], Optional[int]]:
+            # TODO docs
+            """
+            :param start: inclusive left-border
+            :param stop: non-inclusive right-border
+            :return: !!!Both borders are inclusive!!!
+            >>> from itertools import starmap
+            >>> ranges = [(2, 3), (5, 6), (7, 9), (11, 15), (19, 30)]
+            >>> regions = list(starmap(Interval, ranges))
+            >>> annotation = Annotation(regions)
+            >>> annotation.borders(4, 17)
+            (1, 3)
+            >>> annotation.borders(2, 12)
+            (0, 3)
+            >>> annotation.borders(0, 17)
+            (0, 3)
+            >>> annotation.borders(0, 25)
+            (0, 4)
+            >>> annotation.borders(0, 40)
+            (0, 4)
+            >>> annotation.borders(0, 2)
+            (0, 0)
+            >>> annotation.borders(3, 4)
+            (1, 1)
+            >>> annotation.borders(40, 50)
+            (None, None)
+            """
+            final = self.size - 1
 
-        @tco
-        def right_border(r, idx):
-            if self[idx].contains(r) or (idx == final and self[idx].before(r)):
-                return False, idx
-            if not idx and self[idx].after(r):
-                return False, None
-            if self[idx].before(r) and self[idx+1].after(r):
-                return False, idx
-            return True, (r, (idx // 2 if self[idx].after(r) else
-                              ceil((idx + final) / 2)))
+            @tco
+            def left_border(l, idx):
+                if self[idx].contains(l) or (not idx and self[idx].after(l)):
+                    return False, idx
+                if idx >= final and self[idx].before(l):
+                    return False, None
+                if self[idx].before(l) and self[idx+1].after(l):
+                    return False, idx + 1
+                return True, (l, (idx // 2 if self[idx].after(l) else
+                                  ceil((idx + final) / 2)))
 
-        first = None if not self else left_border(start, final // 2)
-        last = None if first is None else right_border(stop-1, first)
-        return first, last or first
+            @tco
+            def right_border(r, idx):
+                if self[idx].contains(r) or (idx == final and self[idx].before(r)):
+                    return False, idx
+                if not idx and self[idx].after(r):
+                    return False, None
+                if self[idx].before(r) and self[idx+1].after(r):
+                    return False, idx
+                return True, (r, (idx // 2 if self[idx].after(r) else
+                                  ceil((idx + final) / 2)))
+
+            first = None if not self else left_border(start, final // 2)
+            last = None if first is None else right_border(stop-1, first)
+            return first, last or first
 
 
 if __name__ == "__main__":

9a33f08daeabd9d0af2887f6e657f36546968502
Ilia Korvigo <ilia.korvigo@gmail.com>
2017-08-24 12:56:38 +0300

    Making attribute names consistent with Python's ranges in Interval objects

--- chempred/annotation.py
+++ chempred/annotation.py
@@ -2,7 +2,6 @@ from typing import List, Tuple, Sequence, Optional, overload
 from numbers import Integral
 from math import ceil
 
-import numpy as np
 from fn.recur import tco
 
 
@@ -12,15 +11,15 @@ class Interval:
     The intervals are non-inclusive on the right side, like Python's `range`.
     """
 
-    __slots__ = ("start", "end")
+    __slots__ = ("start", "stop")
 
-    def __init__(self, start: Integral, end: Integral):
+    def __init__(self, start: Integral, stop: Integral):
         self.start = start
-        self.end = end
+        self.stop = stop
 
     def __repr__(self) -> str:
-        return "{}(start={}, end={})".format(
-            type(self).__name__, self.start, self.end
+        return "{}(start={}, stop={})".format(
+            type(self).__name__, self.start, self.stop
         )
 
     def __contains__(self, point: int) -> bool:
@@ -30,40 +29,40 @@ class Interval:
         return self.start < other.start
 
     def before(self, point: Integral) -> bool:
-        return self.end <= point
+        return self.stop <= point
 
     def after(self, point: Integral) -> bool:
         return self.start > point
 
     def contains(self, point: Integral) -> bool:
-        return self.start <= point < self.end
+        return self.start <= point < self.stop
 
 
 class Token(Interval):
 
-    __slots__ = ("start", "end", "text")
+    __slots__ = ("start", "stop", "text")
 
-    def __init__(self, start: Integral, end: Integral, text: Integral):
-        super().__init__(start, end)
-        self.end = text
+    def __init__(self, start: Integral, stop: Integral, text: Integral):
+        super().__init__(start, stop)
+        self.stop = text
 
     def __repr__(self) -> str:
-        return "{}(start={}, end={}, text={})".format(
-            type(self).__name__, self.start, self.end, self.text
+        return "{}(start={}, stop={}, text={})".format(
+            type(self).__name__, self.start, self.stop, self.text
         )
 
 
 class ClassifiedInterval(Interval):
 
-    __slots__ = ("start", "end", "cls")
+    __slots__ = ("start", "stop", "cls")
 
-    def __init__(self, start: Integral, end: Integral, cls: Integral):
-        super().__init__(start, end)
+    def __init__(self, start: Integral, stop: Integral, cls: Integral):
+        super().__init__(start, stop)
         self.cls = cls
 
     def __repr__(self) -> str:
-        return "{}(start={}, end={}, cls={})".format(
-            type(self).__name__, self.start, self.end, self.cls
+        return "{}(start={}, stop={}, cls={})".format(
+            type(self).__name__, self.start, self.stop, self.cls
         )
 
 
@@ -89,9 +88,9 @@ class Annotation:
         >>> regions = list(starmap(Interval, ranges))
         >>> annotation = Annotation(regions)
         >>> annotation[0]
-        Interval(start=2, end=3)
+        Interval(start=2, stop=3)
         >>> annotation[-1]
-        Interval(start=19, end=30)
+        Interval(start=19, stop=30)
         >>> annotation["a"]
         Traceback (most recent call last):
         ...
@@ -126,7 +125,7 @@ class Annotation:
         >>> ranges = [(2, 3), (5, 6), (7, 9), (11, 15), (19, 30)]
         >>> regions = list(starmap(Interval, ranges))
         >>> annotation = Annotation(regions)
-        >>> [(i.start, i.end) for i in annotation.lookup(4, 17)] == ranges[1:4]
+        >>> [(i.start, i.stop) for i in annotation.lookup(4, 17)] == ranges[1:4]
         True
         >>> annotation.lookup(0, 2)
         []
@@ -137,60 +136,60 @@ class Annotation:
         return self._regions[first:last + 1]
 
     def borders(self, start: Integral, stop: Integral) \
-                -> Tuple[Optional[int], Optional[int]]:
-            # TODO docs
-            """
-            :param start: inclusive left-border
-            :param stop: non-inclusive right-border
-            :return: !!!Both borders are inclusive!!!
-            >>> from itertools import starmap
-            >>> ranges = [(2, 3), (5, 6), (7, 9), (11, 15), (19, 30)]
-            >>> regions = list(starmap(Interval, ranges))
-            >>> annotation = Annotation(regions)
-            >>> annotation.borders(4, 17)
-            (1, 3)
-            >>> annotation.borders(2, 12)
-            (0, 3)
-            >>> annotation.borders(0, 17)
-            (0, 3)
-            >>> annotation.borders(0, 25)
-            (0, 4)
-            >>> annotation.borders(0, 40)
-            (0, 4)
-            >>> annotation.borders(0, 2)
-            (0, 0)
-            >>> annotation.borders(3, 4)
-            (1, 1)
-            >>> annotation.borders(40, 50)
-            (None, None)
-            """
-            final = self.size - 1
+            -> Tuple[Optional[int], Optional[int]]:
+        # TODO docs
+        """
+        :param start: inclusive left-border
+        :param stop: non-inclusive right-border
+        :return: both borders are inclusive
+        >>> from itertools import starmap
+        >>> ranges = [(2, 3), (5, 6), (7, 9), (11, 15), (19, 30)]
+        >>> regions = list(starmap(Interval, ranges))
+        >>> annotation = Annotation(regions)
+        >>> annotation.borders(4, 17)
+        (1, 3)
+        >>> annotation.borders(2, 12)
+        (0, 3)
+        >>> annotation.borders(0, 17)
+        (0, 3)
+        >>> annotation.borders(0, 25)
+        (0, 4)
+        >>> annotation.borders(0, 40)
+        (0, 4)
+        >>> annotation.borders(0, 2)
+        (0, 0)
+        >>> annotation.borders(3, 4)
+        (1, 1)
+        >>> annotation.borders(40, 50)
+        (None, None)
+        """
+        final = self.size - 1
 
-            @tco
-            def left_border(l, idx):
-                if self[idx].contains(l) or (not idx and self[idx].after(l)):
-                    return False, idx
-                if idx >= final and self[idx].before(l):
-                    return False, None
-                if self[idx].before(l) and self[idx+1].after(l):
-                    return False, idx + 1
-                return True, (l, (idx // 2 if self[idx].after(l) else
-                                  ceil((idx + final) / 2)))
+        @tco
+        def left_border(l, idx):
+            if self[idx].contains(l) or (not idx and self[idx].after(l)):
+                return False, idx
+            if idx >= final and self[idx].before(l):
+                return False, None
+            if self[idx].before(l) and self[idx+1].after(l):
+                return False, idx + 1
+            return True, (l, (idx // 2 if self[idx].after(l) else
+                              ceil((idx + final) / 2)))
 
-            @tco
-            def right_border(r, idx):
-                if self[idx].contains(r) or (idx == final and self[idx].before(r)):
-                    return False, idx
-                if not idx and self[idx].after(r):
-                    return False, None
-                if self[idx].before(r) and self[idx+1].after(r):
-                    return False, idx
-                return True, (r, (idx // 2 if self[idx].after(r) else
-                                  ceil((idx + final) / 2)))
+        @tco
+        def right_border(r, idx):
+            if self[idx].contains(r) or (idx == final and self[idx].before(r)):
+                return False, idx
+            if not idx and self[idx].after(r):
+                return False, None
+            if self[idx].before(r) and self[idx+1].after(r):
+                return False, idx
+            return True, (r, (idx // 2 if self[idx].after(r) else
+                              ceil((idx + final) / 2)))
 
-            first = None if not self else left_border(start, final // 2)
-            last = None if first is None else right_border(stop-1, first)
-            return first, last or first
+        first = None if not self else left_border(start, final // 2)
+        last = None if first is None else right_border(stop-1, first)
+        return first, last or first
 
 
 if __name__ == "__main__":

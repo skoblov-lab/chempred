12bdb294d0539d6712891194be50c1ddaa61ffe3
Ilia Korvigo <ilia.korvigo@gmail.com>
2017-08-26 13:49:00 +0300

    Improved documentation and tests

--- chempred/encoding.py
+++ chempred/encoding.py
@@ -16,7 +16,7 @@ from chempred.chemdner import Annotation
 MAXCHAR = 127
 
 
-def encode_sample_chars(text: Text, span: Interval, dtype=np.int32) \
+def encode_text(text: Text, span: Interval, dtype=np.int32) \
         -> np.ndarray:
     # TODO tests
     """
@@ -30,19 +30,27 @@ def encode_sample_chars(text: Text, span: Interval, dtype=np.int32) \
         raise ValueError("`dtype` must be integral")
     encoded = np.fromiter(map(ord, text[span.start:span.stop]),
                           dtype, len(span))
-    encoded[encoded >= MAXCHAR] = MAXCHAR
+    encoded[encoded > MAXCHAR] = MAXCHAR
     return encoded
 
 
-def encode_sample_classes(span: Interval, annotation: Annotation, default=0,
-                          dtype=np.int32) -> np.ndarray:
-    # TODO tests
-    # TODO docs
+def encode_annotation(span: Interval, annotation: Annotation, default=0,
+                      dtype=np.int32) -> np.ndarray:
     """
     :param span: sample's span
+    :param annotation: annotated intervals
+    :param default: default value for unannotated regions
     :param dtype: output data type; it must be an integral numpy dtype
     :return: an integer array
-
+    >>> anno = Intervals([Interval(3, 10, 1), Interval(12, 15, 2)])
+    >>> samples = [Interval(0, 10), Interval(2, 10), Interval(2, 20),
+    ...            Interval(11, 16)]
+    >>> encoded = [encode_annotation(sample, anno) for sample in samples]
+    >>> all(len(s) == len(e) for s, e in zip(samples, encoded))
+    True
+    >>> all(sum(e) == sum(len(i) * i.data for i in anno.within(s.start, s.stop))
+    ...     for s, e in zip(samples, encoded))
+    True
     """
     if not np.issubdtype(dtype, np.int):
         raise ValueError("`dtype` must be integral")
@@ -50,7 +58,7 @@ def encode_sample_classes(span: Interval, annotation: Annotation, default=0,
     encoded = np.repeat([default], len(span)).astype(dtype)
     offset = span.start
     for interval in intervals:
-        encoded[interval.start-offset:interval.end-offset] = interval.data
+        encoded[interval.start-offset:interval.stop-offset] = interval.data
     return encoded
 
 

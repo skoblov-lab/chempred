38bc270acf8cdd14144a59dc0cc39e2d2dcdd1bb
Ilia Korvigo <ilia.korvigo@gmail.com>
2017-10-08 16:20:13 +0300

    adding more metrics

--- sciner/objectives.py
+++ sciner/objectives.py
@@ -1,11 +1,11 @@
 from keras import backend as K
 from keras.losses import binary_crossentropy
 
-from sciner.metrics import sensitivity
+from sciner.metrics import recall
 
 
 def binary_crossentropy_with_sensitivity(y_true, y_pred):
-    sens = sensitivity(y_true, y_pred)
+    sens = recall(y_true, y_pred)
     bce = binary_crossentropy(y_true, y_pred)
     return bce + K.clip(1 - sens, K.epsilon(), 1 - K.epsilon())
 

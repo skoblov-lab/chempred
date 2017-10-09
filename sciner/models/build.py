16a5544a7518e94d1202ee333057fef51d0ddca8
Ilia Korvigo <ilia.korvigo@gmail.com>
2017-10-09 21:04:29 +0300

    adding embedding builders

--- sciner/models/build.py
+++ sciner/models/build.py
@@ -7,18 +7,15 @@ their predictions
 from functools import reduce
 from typing import Sequence, Tuple, Optional, Union, Callable
 
-from keras import layers
-
-from sciner.preprocessing import encoding
-
-NCHAR = encoding.MAXCHAR + 1
+from keras import layers, backend as K
+import numpy as np
 
 
 def build_cnn(nfilters: Sequence[int],
               filter_width: Union[int, Sequence[int]],
               dropout: Union[Optional[float], Sequence[Optional[float]]]=None,
               padding: Union[str, Sequence[str]]="same",
-              name_template: str="l_conv_{}") \
+              name_template: str="conv{}") \
         -> Callable:
     # TODO extend documentation
     # TODO more tests
@@ -27,7 +24,6 @@ def build_cnn(nfilters: Sequence[int],
     :param nfilters:
     :param filter_width:
     :return:
-    >>> conv = build_cnn([30, 30], 5)
     """
     def stack_conv(prev, param: Tuple[str, int, int, float, str]):
         name, nfilt, kern_size, drop_p, pad = param
@@ -61,15 +57,16 @@ def build_rnn(nsteps: Sequence[int],
               bidirectional: Union[Optional[str], Sequence[Optional[str]]]=None,
               stateful=False, layer=layers.LSTM) -> Callable:
     # TODO extend documentation
+    # TODO add name template argument
     # TODO tests
     """
     :param nsteps:
-    :param lstm_inp_drop:
-    :param lstm_rec_drop:
+    :param inp_drop:
+    :param rec_drop:
     :param bidirectional:
     :param stateful: use stateful RNN-cells
+    :param layer: a recurrent layer to use
     :return:
-    >>> rec = build_rnn([200, 200], 0.1, 0.1, True)
     """
 
     def stack_layers(prev, param: Tuple[str, int, float, float, str]):
@@ -83,8 +80,9 @@ def build_rnn(nsteps: Sequence[int],
                        return_sequences=True, stateful=stateful)
         return (layers.Bidirectional(layer_, bidir) if bidir else layer_)(prev)
 
-    bi = (bidirectional if isinstance(bidirectional, Sequence) and not isinstance(bidirectional, str) else
-          [bidirectional] * len(nsteps))
+    bidir_is_seq = (isinstance(bidirectional, Sequence)
+                    and not isinstance(bidirectional, str))
+    bi = (bidirectional if bidir_is_seq else [bidirectional] * len(nsteps))
     inp_drop = (inp_drop if isinstance(inp_drop, Sequence) else
                 [inp_drop or 0] * len(nsteps))
     rec_drop = (rec_drop if isinstance(rec_drop, Sequence) else
@@ -94,13 +92,42 @@ def build_rnn(nsteps: Sequence[int],
         raise ValueError("Parameter sequences have different length")
 
     def rec(incomming):
-        rec_names = ("rec_{}".format(i) for i in range(1, len(nsteps)+1))
-        parameters = zip(rec_names, nsteps, map(float, inp_drop),
-                         map(float, rec_drop), bi)
+        rec_names = ("rec{}".format(i) for i in range(1, len(nsteps)+1))
+        parameters = zip(rec_names, nsteps, inp_drop, rec_drop, bi)
         rnn = reduce(stack_layers, parameters, incomming)
         return rnn
 
     return rec
+
+
+def build_word_embeddings(embeddings: np.ndarray, maxsteps, mask: bool):
+    # TODO docs
+    words = layers.Input(shape=(None, maxsteps), dtype="int32")
+    embeddings = layers.embeddings.Embedding(input_dim=embeddings.shape[0],
+                                             output_dim=embeddings.shape[1],
+                                             mask_zero=mask,
+                                             weights=[embeddings])(words)
+    return embeddings
+
+
+def build_char_embeddings(nchar: int, embsize: int, units: int, maxsteps: int,
+                          maxlen: bool, mask: bool, layer=layers.LSTM):
+    # TODO docs
+    chars = layers.Input(shape=(None, maxsteps, maxlen), dtype="int32")
+    embeddings = layers.embeddings.Embedding(input_dim=nchar,
+                                             output_dim=embsize,
+                                             mask_zero=mask)(chars)
+    shape = K.shape(embeddings)
+    embeddings = layers.Lambda(
+        lambda x: K.reshape(x, shape=(-1, shape[-2], embsize)))(embeddings)
+
+    forward = layer(units, return_state=True)(embeddings)[-2]
+    reverse = layer(units, return_state=True, go_backwards=True)(embeddings)[-2]
+    embeddings = layers.concatenate([forward, reverse], axis=-1)
+    # shape = (batch size, max sentence length, char hidden size)
+    embeddings = layers.Lambda(
+        lambda x: K.reshape(x, shape=[-1, shape[1], 2 * units]))(embeddings)
+    return embeddings
 
 
 if __name__ == "__main__":

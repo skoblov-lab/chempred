a041f9a06ad2fd517265501529cd9afc6e8ee51e
Ilia Korvigo <ilia.korvigo@gmail.com>
2017-10-11 00:07:45 +0300

    Reimplementing the F1 callback

--- sciner/models/metrics.py
+++ sciner/models/metrics.py
@@ -1,92 +1,47 @@
+"""
+
+
+"""
+
+from typing import Tuple, Sequence, Text, Callable
+
 from keras import callbacks, backend as K
+from sklearn import metrics
+from fn.func import identity
 import numpy as np
 
 
 class F1score(callbacks.Callback):
 
-    def __init__(self, valid_steps, valid_batches, preprocessor=None):
-        super(F1score, self).__init__()
-        self.valid_steps = valid_steps
-        self.valid_batches = valid_batches
-        self.p = preprocessor
+    modes = ("binary", "micro", "macro", "weighted", "samples")
+
+    def __init__(self,
+                 inputs: Sequence[np.ndarray],
+                 output: Sequence[np.ndarray],
+                 labels: Sequence[int],
+                 mode: Text,
+                 batchsize: int,
+                 transform: Callable[[np.ndarray], np.ndarray]=identity):
+        if mode not in self.modes:
+            raise ValueError("`mode` must be one of {}".format(self.modes))
+
+        super().__init__()
+        self.inputs = inputs
+        self.output = output
+        self.labels = labels
+        self.epoch = None
+        self.best = -float("inf")
+        self.mode = mode
+        self.batchsize = batchsize
+        self.transform = transform
 
     def on_epoch_end(self, epoch, logs=None):
-        logs = logs or {}
-        correct_preds, total_correct, total_preds = 0., 0., 0.
-        for i, (data, label) in enumerate(self.valid_batches):
-            if i == self.valid_steps:
-                break
-            y_true = label
-            y_true = np.argmax(y_true, -1)
-            sequence_lengths = data[-1]  # shape of (batch_size, 1)
-            sequence_lengths = np.reshape(sequence_lengths, (-1,))
-            #y_pred = np.asarray(self.model_.predict(data, sequence_lengths))
-            y_pred = self.model.predict_on_batch(data)
-            y_pred = np.argmax(y_pred, -1)
-
-            y_pred = [self.p.inverse_transform(y[:l]) for y, l in zip(y_pred, sequence_lengths)]
-            y_true = [self.p.inverse_transform(y[:l]) for y, l in zip(y_true, sequence_lengths)]
-
-            a, b, c = self.count_correct_and_pred(y_true, y_pred, sequence_lengths)
-            correct_preds += a
-            total_preds += b
-            total_correct += c
-
-        f1 = self._calc_f1(correct_preds, total_correct, total_preds)
-        print(" - f1: {:04.2f}".format(f1 * 100))
-        logs["f1"] = f1
-
-    def _calc_f1(self, correct_preds, total_correct, total_preds):
-        p = correct_preds / total_preds if correct_preds > 0 else 0
-        r = correct_preds / total_correct if correct_preds > 0 else 0
-        f1 = 2 * p * r / (p + r) if correct_preds > 0 else 0
-        return f1
-
-    def count_correct_and_pred(self, y_true, y_pred, sequence_lengths):
-        correct_preds, total_correct, total_preds = 0., 0., 0.
-        for lab, lab_pred, length in zip(y_true, y_pred, sequence_lengths):
-            lab = lab[:length]
-            lab_pred = lab_pred[:length]
-
-            lab_chunks = set(get_entities(lab))
-            lab_pred_chunks = set(get_entities(lab_pred))
-
-            correct_preds += len(lab_chunks & lab_pred_chunks)
-            total_preds += len(lab_pred_chunks)
-            total_correct += len(lab_chunks)
-        return correct_preds, total_correct, total_preds
-
-
-def get_entities(seq):
-    """Gets entities from sequence.
-
-    Args:
-        seq (list): sequence of labels.
-
-    Returns:
-        list: list of (chunk_type, chunk_start, chunk_end).
-
-    Example:
-        >>> seq = ['B-PER', 'I-PER', 'O', 'B-LOC']
-        >>> print(get_entities(seq))
-        [('PER', 0, 2), ('LOC', 3, 4)]
-    """
-    i = 0
-    chunks = []
-    seq = seq + ['O']  # add sentinel
-    types = [tag.split('-')[-1] for tag in seq]
-    while i < len(seq):
-        if seq[i].startswith('B'):
-            for j in range(i+1, len(seq)):
-                if seq[j].startswith('I') and types[j] == types[i]:
-                    continue
-                break
-            chunks.append((types[i], i, j))
-            i = j
-        else:
-            i += 1
-    return chunks
-
+        self.epoch = epoch
+        pred = self.transform(self.model.predict(self.inputs, self.batchsize))
+        score = metrics.f1_score(self.output, pred, self.labels, average=self.mode)
+        if score > self.best:
+            print("F1 improved from {} to {}".format(self.best, score))
+            self.best = score
 
 
 def precision(y_true, y_pred):

4005681088d0695e3b89de08b5812d73fd299eb1
Ilia Korvigo <ilia.korvigo@gmail.com>
2017-10-10 03:24:21 +0300

    refactoring

--- sciner/corpora/genia.py
+++ sciner/corpora/genia.py
@@ -10,7 +10,7 @@ from xml.etree.ElementTree import Element, parse
 from fn import F
 from pyrsistent import v, pvector
 
-from sciner.corpora.abstract import AbstractAnnotation, AbstractText, ClassMapping, \
+from sciner.corpora.corpora import AbstractAnnotation, AbstractText, ClassMapping, \
     AnnotationError, ClassifiedInterval
 from sciner.intervals import Interval
 
